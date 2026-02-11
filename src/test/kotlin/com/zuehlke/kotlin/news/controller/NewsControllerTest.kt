04a6bade0f4dad5974fc74e5e2c4edc00f0895fa
Graf, Manuel <manuel.graf@zuhlke.com>
2026-02-11 14:44:43 +0100

    fix imports

--- src/test/kotlin/com/zuehlke/kotlin/news/controller/NewsControllerTest.kt
+++ src/test/kotlin/com/zuehlke/kotlin/news/controller/NewsControllerTest.kt
@@ -3,9 +3,9 @@ package com.zuehlke.kotlin.news.controller
 import com.zuehlke.kotlin.news.data.service.NewsServiceRemote
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -17,7 +17,7 @@ class NewsControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @MockBean
+    @MockitoBean
     lateinit var newsServiceRemote: NewsServiceRemote
 
     @Test
@@ -25,7 +25,7 @@ class NewsControllerTest {
         mockMvc.perform(get("/api/testnews")).andExpect(status().isOk)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(
-                content().json (
+                content().json(
                     """
                      {
                       "status": "",
